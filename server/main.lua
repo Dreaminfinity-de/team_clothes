fa9fc3130ff6f4da957816232bfc16327ebe4ebb
Grewo <grewo@t-online.de>
2021-05-08 01:56:26 +0200

    change config - clothes

--- server/main.lua
+++ server/main.lua
@@ -2,7 +2,7 @@ ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-RegisterCommand("support", function(source)
+RegisterCommand("admin", function(source)
 
 	local xPlayer = ESX.GetPlayerFromId(source)
 	
