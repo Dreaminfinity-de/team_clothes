d2003380877ab07429770ea6681748bc8f57ca44
Grewo <grewo@t-online.de>
2021-06-29 18:32:43 +0200

    Change clothesfor mod

--- config.lua
+++ config.lua
@@ -40,4 +40,22 @@ Config.skins = {
 			['shoes_1'] = 27,   ['shoes_2'] = 0
 		}
 	},
+	mod = {
+		male = {
+			['tshirt_1'] = 15,  ['tshirt_2'] = 0,
+			['torso_1'] = 22,   ['torso_2'] = 1,
+			['arms'] = 41,
+			['bproof_1'] = 18,	['bproof_2'] = 3,
+			['pants_1'] = 31,   ['pants_2'] = 0,
+			['shoes_1'] = 25,   ['shoes_2'] = 0
+		},
+		female = {
+			['tshirt_1'] = 36,  ['tshirt_2'] = 1,
+			['torso_1'] = 48,   ['torso_2'] = 0,
+			['arms'] = 44,
+			['bproof_1'] = 18,	['bproof_2'] = 3,
+			['pants_1'] = 30,   ['pants_2'] = 0,
+			['shoes_1'] = 27,   ['shoes_2'] = 0
+		}
+	},
 }
