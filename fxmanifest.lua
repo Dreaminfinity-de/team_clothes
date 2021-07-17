334602bcef46ed1968770805ebf5851867c52f17
Grewo <grewo@t-online.de>
2021-07-18 01:26:12 +0200

    change fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -10,14 +10,14 @@ server_scripts {
 	'@mysql-async/lib/MySQL.lua',
 	'@es_extended/locale.lua',
 	'config.lua',
-	'server/main.lua',
+	'server/server.lua',
 	'de.lua',
 }
 
 client_scripts {
 	'@es_extended/locale.lua',
 	'config.lua',
-	'client/main.lua',
+	'client/client.lua',
 	'de.lua',
 }
 
