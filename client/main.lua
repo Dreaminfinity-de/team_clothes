3f38bb52086e832607427d06f8b40943c77d1c72
EThunder <Mkerner2001@web.de>
2021-08-18 19:53:57 +0200

    Bugfix: tow supporter on the same time not worked
    Update: better godmode on duty

--- client/main.lua
+++ client/main.lua
@@ -1,6 +1,5 @@
 ESX = nil
-oldjobname = nil
-oldjobgrade = nil
+onduty = false
 
 Citizen.CreateThread(function()
 	while ESX == nil do
@@ -9,84 +8,37 @@ Citizen.CreateThread(function()
 	end
 end)
 
-RegisterNetEvent("team_clothes:changeSkin")
-AddEventHandler("team_clothes:changeSkin", function(group)
+RegisterNetEvent('team_clothes:setOnduty')
+AddEventHandler('team_clothes:setOnduty', function(group)
+	client = ESX.GetPlayerData()
 
+	TriggerEvent('skinchanger:getSkin', function(skin)
+		onduty = true
 
-	changeSkin_func(group)
+		TriggerServerEvent('team_clothes:saveSkin', skin)
+
+		playerPed = GetPlayerPed(-1)
+		Citizen.CreateThread(function()
+			while onduty do
+				Citizen.Wait(0)
+				SetEntityInvincible(playerPed, true)
+			end
+		end)
+
+		if skin.sex == 0 then
+			TriggerEvent('skinchanger:loadClothes', skin, Config.skins[group].male)
+		elseif skin.sex == 1 then
+			TriggerEvent('skinchanger:loadClothes', skin, Config.skins[group].female)
+		else
+			ESX.ShowNotification('~r~ Error - Sex undefined!!!')
+		end
+	
+	end)
 end)
 
-RegisterNetEvent("team_clothes:changeJob")
-AddEventHandler("team_clothes:changeJob", function()
-
-    changeJob_func()
-
+RegisterNetEvent('team_clothes:setOffduty')
+AddEventHandler('team_clothes:setOffduty', function(group)
+	onduty = false
+	playerPed = GetPlayerPed(-1)
+	SetEntityInvincible(playerPed, false)
 end)
-
-
-local offduty = nil
-
-
-
-
-function changeSkin_func(group)
-	if offduty == nil then
-		client = ESX.GetPlayerData()
-		oldjobname = client.job.name
-		oldjobgrade = client.job.grade
-
-		TriggerServerEvent('setjob', group)
-		
-
-		TriggerEvent('skinchanger:getSkin', function(skin)
-			SetEntityProofs(GetPlayerPed(-1), true, true, true, true, true, true, true, true)
-			if skin.sex == 0 then
-				TriggerEvent('skinchanger:loadClothes', skin, Config.skins[group].male)
-				ESX.ShowNotification(_U('skin_onduty'))
-				offduty = skin
-				TriggerServerEvent('offduty', false)
-			elseif skin.sex == 1 then
-				TriggerEvent('skinchanger:loadClothes', skin, Config.skins[group].female)
-				ESX.ShowNotification(_U('skin_onduty'))
-				offduty = skin
-				TriggerServerEvent('offduty', false)
-			else
-				ESX.ShowNotification('~r~ Error - Sex undefinied!!!')
-			end
-		
-		end)
-	
-	else
-		TriggerServerEvent('setjobback', oldjobname, oldjobgrade)
-		oldjobname = nil
-		oldjobgrade = nil
-
-		TriggerEvent('skinchanger:getSkin', function(skin)
-			SetEntityProofs(GetPlayerPed(-1), false, false, false, false, false, false, false, false)
-			if skin.sex == 0 then
-				TriggerEvent('skinchanger:loadClothes', skin, offduty)
-				ESX.ShowNotification(_U('skin_offduty'))
-				offduty = nil
-				TriggerServerEvent('offduty', true)
-			elseif skin.sex == 1 then
-				TriggerEvent('skinchanger:loadClothes', skin, offduty)
-				ESX.ShowNotification(_U('skin_offduty'))
-				offduty = nil
-				TriggerServerEvent('offduty', true)
-			else
-				ESX.ShowNotification('~r~ Error - Sex undefinied!!!')
-			end
-		
-		end)
-	end
-end
-
-function changeJob_func()
-	
-	if offduty == nil then
-		
-		return true
-		
-	end
-
-end
--- server/main.lua
+++ server/main.lua
@@ -3,22 +3,35 @@ offduty = true
 oldjobname = nil
 oldjobgrade = nil
 
+onduty = {}
+
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+RegisterNetEvent('team_clothes:saveSkin')
+AddEventHandler('team_clothes:saveSkin', function(skin)
+	if onduty[source] ~= nil then
+		onduty[source].skin = skin
+	end
+end)
+
 
 
 for k,v in pairs(Config.skins) do
+	command = k
 	RegisterCommand(k, function(source)
 
 		local xPlayer = ESX.GetPlayerFromId(source)
 		
 		if source > 0 then
-			if xPlayer.getGroup() == 'superadmin' or xPlayer.getGroup() == k then
-
-				TriggerClientEvent("team_clothes:changeSkin", source, k)
-				
-				--print("You are not console.")
+			--print("You are not console.")
+			if isOnduty(source) then
+				setOffduty(source)
 			else
-				xPlayer.showNotification(_U('command_denied'))
+				if xPlayer.getGroup() == 'superadmin' or xPlayer.getGroup() == command then
+					setOnduty(source, k)
+				else
+					xPlayer.showNotification(_U('command_denied'))
+				end
 			end
 		else
 			--print("This is console!")
@@ -26,21 +39,36 @@ for k,v in pairs(Config.skins) do
 	end, false) -- this true bool means that the user cannot execute the command unless they have the 'command.commandName' ACL object allowed to one of their identifiers.
 end
 
-
-
-RegisterNetEvent('offduty')
-AddEventHandler('offduty', function(k)
-	offduty = k
+AddEventHandler('onResourceStop', function(resourceName)
+	if (GetCurrentResourceName() == resourceName) then
+		for k,v in pairs(onduty) do
+			setOffduty(k)
+		end
+	end
 end)
 
-RegisterNetEvent('setjob')
-AddEventHandler('setjob', function(group)
+
+
+function isOnduty(playerid)
+	return onduty[playerid] ~= nil
+end
+
+function setOffduty(source)
 	local xPlayer = ESX.GetPlayerFromId(source)
+	xPlayer.setJob(onduty[source].job, onduty[source].grade)
+	TriggerClientEvent('skinchanger:loadClothes', source, onduty[source].skin, onduty[source].skin)
+	onduty[source] = nil
+	TriggerClientEvent("team_clothes:setOffduty", source)
+
+	xPlayer.showNotification(_U('skin_offduty'))
+end
+
+function setOnduty(source, group)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	
+	onduty[source] = { job = xPlayer.getJob().name, grade = xPlayer.getJob().grade }
 	xPlayer.setJob('team', Config.skins[group].jobgrade)
-end)
-
-RegisterNetEvent('setjobback')
-AddEventHandler('setjobback', function(name, grade)
-	local xPlayer = ESX.GetPlayerFromId(source)
-	xPlayer.setJob(name, grade)
-end)
+	TriggerClientEvent("team_clothes:setOnduty", source, group)
+	
+	xPlayer.showNotification(_U('skin_onduty'))
+end
